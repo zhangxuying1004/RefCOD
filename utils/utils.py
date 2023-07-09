c177cbcb19efd011fda052e68d6902e4377cca4b
张旭迎 <47649909+zhangxuying1004@users.noreply.github.com>
2023-07-09 14:33:05 +0800

    Create utils.py

---
+++ utils/utils.py
@@ -0,0 +1,41 @@
+import os
+
+import torch
+import torch.nn.functional as F
+
+
+def structure_loss(pred, mask):
+    """
+    loss function (ref: F3Net-AAAI-2020)
+    """
+    weit = 1 + 5 * torch.abs(F.avg_pool2d(mask, kernel_size=31, stride=1, padding=15) - mask)
+    wbce = F.binary_cross_entropy_with_logits(pred, mask, reduce='none')
+    wbce = (weit * wbce).sum(dim=(2, 3)) / weit.sum(dim=(2, 3))
+
+    pred = torch.sigmoid(pred)
+    inter = ((pred * mask) * weit).sum(dim=(2, 3))
+    union = ((pred + mask) * weit).sum(dim=(2, 3))
+    wiou = 1 - (inter + 1) / (union - inter + 1)
+    return (wbce + wiou).mean()
+
+
+def clip_gradient(optimizer, grad_clip):
+    """
+    For calibrating misalignment gradient via cliping gradient technique
+    :param optimizer:
+    :param grad_clip:
+    :return:
+    """
+    for group in optimizer.param_groups:
+        for param in group['params']:
+            if param.grad is not None:
+                param.grad.data.clamp_(-grad_clip, grad_clip)
+              
+
+def load_model_params(model, params_path):
+    assert os.path.exists(params_path)
+    checkpoints = torch.load(params_path)
+    # print(checkpoints['epoch'])
+
+    model.load_state_dict(checkpoints['state_dict'])
+    return model
